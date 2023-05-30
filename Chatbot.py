22450989f2865dc600540ce6d5e69e295b4b8d06
Srushti kakade <85052036+SrushtiKakade28@users.noreply.github.com>
2023-05-30 11:23:56 +0530

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -23,3 +23,40 @@ e = Entry(root, width=100)
 e.grid(row=1, column=0)
 send = Button(root, text="Send", command=send).grid(row=1, column=1)
 root.mainloop()
+
+
+
+
+
+orrr
+def greet():
+    print("Chatbot: Hello! How can I assist you today?")
+
+
+def goodbye():
+    print("Chatbot: Thank you for chatting with me. Goodbye!")
+
+
+def process_input(user_input):
+    if user_input.lower() == "hello":
+        print("Chatbot: Hi there!")
+    elif user_input.lower() == "help":
+        print("Chatbot: I'm here to help you. How can I assist you?")
+    elif user_input.lower() == "bye":
+        goodbye()
+        return True
+    else:
+        print("Chatbot: I'm sorry, I didn't understand that. Can you please rephrase or ask a different question?")
+
+
+def start_chat():
+    greet()
+    while True:
+        user_input = input("User: ")
+        should_exit = process_input(user_input)
+        if should_exit:
+            break
+
+
+# Start the chatbot
+start_chat()
